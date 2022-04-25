6424016b302ce247e190e40a9fa5fb9b6bbd76a7
James Goodall <xfdg93@ncc1.clients.dur.ac.uk>
2022-04-25 14:26:16 +0100

    added run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-docker run -m 10G --rm -it --runtime=nvidia  --ipc=host -e NVIDIA_VISIBLE_DEVICES=0 -v ~/projects/cv/:/workspace cv python cv.py
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -o stdout.out
+#SBATCH -c 4
+#SBATCH --gres=gpu
+#SBATCH -p ug-gpu-small
+#SBATCH --qos=debug
+#SBATCH -t 2:00:00
+#SBATCH --job-name=ACV
+#SBATCH --mem=6G
+
+source /etc/profile
+singularity exec --nv ../cv.tif python -u cv.py
